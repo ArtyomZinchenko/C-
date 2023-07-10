4fae49e527d05cc2c73ae0b0169284c3cf3e8e34
ArtyomZinchenko <artyom.zinchenko@gmail.com>
2023-07-10 08:01:11 +0200

    Example006 simple if statement, find maximum

---
+++ example006/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 2;
+int c = 5;
+int d = 23;
+int e = 55;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine("The maximum number is " + max);
