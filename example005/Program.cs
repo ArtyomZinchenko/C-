da5c6f20a52d6332a3bf440aca2bb20398c251ea
ArtyomZinchenko <artyom.zinchenko@gmail.com>
2023-07-10 07:51:52 +0200

    if-else statements

---
+++ example005/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Insert your name, please: ");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "artyom"){
+
+Console.WriteLine("Hurray! This is Artyom! Welcome!");
+
+} else {
+
+Console.WriteLine("Hello, " + username);
+
+}
+
