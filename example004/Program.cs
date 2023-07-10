7a44cd95ad8a094b3972228ec6c23fbd9302fac8
ArtyomZinchenko <artyom.zinchenko@gmail.com>
2023-07-10 07:43:30 +0200

    added random numbers

--- example004/Program.cs
+++ example004/Program.cs
@@ -1,5 +1,13 @@
-﻿double numberA = 10;
-double numberB = 12; //this can be either int OR double, depending on what you need.
+﻿//double numberA = 10;
+//double numberB = 12; //this can be either int OR double, depending on what you need.
+
+//Console.WriteLine(numberA / numberB);
+
+
+double numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10);; //this can be either int OR double, depending on what you need.
+Console.WriteLine(numberB);
 
 Console.WriteLine(numberA / numberB);
 
