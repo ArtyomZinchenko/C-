1b46ec22c997386cbf3a9c0fd4f3e4f1cd99782c
ArtyomZinchenko <artyom.zinchenko@gmail.com>
2023-07-10 07:25:12 +0200

    Example2: greet the user

--- example002/Program.cs
+++ example002/Program.cs
@@ -1,4 +1,5 @@
 ﻿Console.Write("Insert your name, please: ");
+Console.WriteLine("Insert your name, please: "); //This writes and jumps to the next line, thus WriteLine
 string username = Console.ReadLine();
 Console.WriteLine("Hi there, " + username);
 
