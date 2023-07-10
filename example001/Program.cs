892dbfb0a9e5dc6e950b488f33a17ca9fedbc444
ArtyomZinchenko <artyom.zinchenko@gmail.com>
2023-07-10 07:02:02 +0200

    Testing new repository

---
+++ example001/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// dotnet new console  -- to start working in this new folder, a new project is initialized
+// dotnet run  -- to execute the code fully
+
+// renamed repository to C-sharp, let's see if push still works
+
+
