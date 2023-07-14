cf8a1b83a3deead3726ff500cc4b82fcf98604d5
ArtyomZinchenko <artyom.zinchenko@gmail.com>
2023-07-14 08:03:16 +0200

    Updated task 3, added validation stage to see whether the input value is at all a day of the week

--- Homework_lecture2/task3/Program.cs
+++ Homework_lecture2/task3/Program.cs
@@ -12,14 +12,22 @@ int dayOfWeek = int.Parse(Console.ReadLine());
 bool isWeekend = IsWeekend(dayOfWeek);
 
 // provide the response based on the isWeekend
-if (isWeekend)
+if (Validate(dayOfWeek))
 {
-    Console.WriteLine("Yes, it's a weekend day!");
+    if (isWeekend)
+    {
+        Console.WriteLine("Yes, it's a weekend day!");
+    }
+    else
+    {
+        Console.WriteLine("No, it's not a weekend day.");
+    }
 }
-else
+else 
 {
-    Console.WriteLine("No, it's not a weekend day.");
+     Console.WriteLine($"Wrong input! {dayOfWeek} is not day of the week! Restrict your selection to first 7 digits above 0!");
 }
+
 
 // Here is the function. It returns 1 = YES, weekend, and 0 = NO, not a weekend. This output is used above to Console.Write the correct reponse
 bool IsWeekend(int dayOfWeek)
@@ -27,6 +35,8 @@ bool IsWeekend(int dayOfWeek)
     return dayOfWeek == 6 || dayOfWeek == 7;
 }
 
+bool Validate(int dayOfWeek)
+{
+    return dayOfWeek>0 && dayOfWeek < 8;
 
-
-
+}
