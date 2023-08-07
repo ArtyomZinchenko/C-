4d1100503416ae69ca18cb3b3c31854bd5946b17
ArtyomZinchenko <artyom.zinchenko@gmail.com>
2023-08-07 15:40:04 +0200

    Added homework 8 on recursions

---
+++ Homework_lecture8/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int result = SumNumbersInRange(1, 20);
+System.Console.WriteLine($"The resulting sum is {result}");
+
+int SumNumbersInRange(int M, int N)
+{
+    if (M == N) return M; // if M equals N, return M
+    if (M > N) return M + SumNumbersInRange(M - 1, N); // if M is greater than N, decrement M
+    return M + SumNumbersInRange(M + 1, N); // if M is less than N, increment M
+}
---
+++ Homework_lecture8/task3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int result = Ackermann(3, 2);
+Console.WriteLine(result);
+
+int Ackermann(int m, int n)
+{
+    // Check if m or n is negative
+    if (m < 0 || n < 0)
+    {
+        Console.WriteLine("Both m and n must be non-negative.");
+        return -999; // I return -999 to indicate an error
+    }
+
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    else // m > 0 and n > 0
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+}
