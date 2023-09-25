76c5c5992f8f1a58079fc428e6c3c0e434ec0b1e
ArtyomZinchenko <artyom.zinchenko@gmail.com>
2023-09-25 11:46:14 +0600

    Lecture 12 on algorithms

---
+++ Homework_lecture_12_algorithms/Program.cs
@@ -0,0 +1,19 @@
+﻿// Learning algorithms; Speaking about classification of difficulty of algorithms (O(n); O(log2(n))
+//
+
+
+
+
+
+Console.Clear();
+Console.Write("Insert a number: ");
+int n = int.Parse(Console.ReadLine()!); // Exclamation mark removes the warning about Null
+
+Console.WriteLine("You entered number: ", n);
+int result = 0;
+
+for (int i = 1; i <= n; i++) //o(n) difficulty = linear algorithm
+{
+    result += i;
+}
+Console.WriteLine($"The sum of numbers from 1 till {n} equals {result}");
